5e8c7b64a762972da9c33036af9d559819129cec
tommelomn <123158609+tommelomn@users.noreply.github.com>
2024-04-17 10:58:39 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 const express = require('express');
 const { exec } = require('child_process');
+const os = require('os');
 const app = express();
 const port = 3000;
 
 let currentProcess = null; // Variable to store the current child process
 
+// Get the username of the current user
+const username = os.userInfo().username;
+
 // Set up the static parameters
-const staticParams = '-s 2600000 -a 1 -x 0'; // Add other static parameters here
-const scriptPath = "C:/PothosSDR/bin/hackrf_transfer.exe";
+const staticParams = '-s 2600000 -f 1575420000 -a 0 -x 0'; // Add other static parameters here
+const scriptPath = "/usr/bin/hackrf_transfer";
 // Define a list to store location parameters
 const locationParameters = [
-    { name: 'brotorvet', path: 'C:/PothosSDR/bin/brotorvet.bin' },
-    { name: 'kiel', path: 'C:/PothosSDR/bin/kiel.bin' }
+    { name: 'brotorvet', path: `/home/${username}/Desktop/gps-sdr-sim-master/brotorvet.bin` },
+    { name: 'kiel', path: `/home/${username}/Desktop/gps-sdr-sim-master/kiel.bin` }
     // Add more locations and parameters as needed
 ];
 
@@ -22,7 +26,7 @@ app.post('/spoof-file', (req, res) => {
     const { index } = req.body;
 
     // Check if the process is running before executing the command
-    checkProcessRunning("hackrf_transfer.exe", (isRunning) => {
+    checkProcessRunning("hackrf_transfer", (isRunning) => {
         if (!isRunning) {
             console.log("\nSpoofing started successfully.")
             executeCommand(res, index);
@@ -34,19 +38,28 @@ app.post('/spoof-file', (req, res) => {
 });
 
 app.post('/create-location-file', (req, res) => {
-    const { filename, lat, lon } = req.body;
+    const { filename, lat, lng } = req.body;
     // For now, assuming filename is the name of the location
-    const newLocation = { name: filename, path: `C:/PothosSDR/bin/${filename}.bin` };
+    const newLocation = { name: filename, path: `/home/${username}/Desktop/gps-sdr-sim-master/${filename}.bin` };
     locationParameters.push(newLocation);
     // Run the command to create the location file
-    createLocationFileInTerminal(filename, lat, lon, res);
+    createLocationFileInTerminal(filename, lat, lng, res);
+});
+
+app.post('/create-motion-location-file', (req, res) => {
+    const { filename, lat, lng, lat2, lng2 } = req.body;
+    // For now, assuming filename is the name of the location
+    const newLocation = { name: filename, path: `/home/${username}/Desktop/gps-sdr-sim-master/${filename}.bin` };
+    locationParameters.push(newLocation);
+    // Run the command to create the location file
+    createMotionLocationFileInTerminal(filename, lat, lng, lat2, lng2, res);
 });
 
 function executeCommand(res, index) {
     // Execute the script with the correct parameter and other static parameters
     const parameter = `-t "${locationParameters[index].path}"`;
     currentProcess = exec(`${scriptPath} ${parameter} ${staticParams}`);
-
+    console.log(currentProcess.pid);
     // Capture stdout from the child process and send it to the client
     console.log("\nProsess-id:", currentProcess.pid, "\n");
     currentProcess.stdout.on('data', (data) => {
@@ -68,17 +81,21 @@ function executeCommand(res, index) {
 }
 
 function checkProcessRunning(processName, callback) {
-    exec(`tasklist /FI "IMAGENAME eq ${processName}"`, (err, stdout, stderr) => {
+    exec(`pgrep -x "${processName}"`, (err, stdout, stderr) => {
         if (err) {
-            console.error(`Error checking process: ${err}`);
+            // Consider the process not running if there's an error
+            callback(false);
             return;
         }
-        callback(stdout.includes(processName));
+        // Process is considered running if stdout is not empty
+        if (stdout && stdout.trim() !== '') {
+            callback(true);
+        }
     });
 }
 
-function createLocationFileInTerminal(filename, lat, lon, res) {
-    const command = `C:/Users/Tommelomn/Desktop/gps-sdr-sim-gui-170216/gps-sdr-sim.exe -e "C:/Users/Tommelomn/Desktop/gps-sdr-sim-gui-170216/brdc0960.24n" -l ${lat},${lon},100 -b 8 -d 25 -o "C:/PothosSDR/bin/${filename}.bin"`;
+function createLocationFileInTerminal(filename, lat, lng, res) {
+    const command = `/home/${username}/Desktop/gps-sdr-sim-master/./gps-sdr-sim -e "/home/${username}/Desktop/gps-sdr-sim-master/brdc1010.24n" -l ${lat},${lng},100 -b 8 -d 25 -o "/home/${username}/Desktop/gps-sdr-sim-master/${filename}.bin"`;
     const createProcess = exec(command);
 
     // Capture stdout from the child process and send it to the client
@@ -95,6 +112,50 @@ function createLocationFileInTerminal(filename, lat, lon, res) {
 
     // Handle the child process exit event
     createProcess.on('exit', (code) => {
+        console.log(`Location file ${filename}.bin created successfully.`);
+        res.end(); // End the response once the process exits
+    });
+}
+
+function createMotionLocationFileInTerminal(filename, lat, lng, lat2, lng2, res) {
+    const command1 = `/home/${username}/Desktop/gps-sdr-sim-master/umc.exe 100 ${lat} ${lng} ${lat2} ${lng2}`;
+    const createProcess1 = exec(command1);
+
+    // Capture stdout from the child process and send it to the client
+    createProcess1.stdout.on('data', (data) => {
+        console.log('stdout:', data);
+        res.write(data);
+    });
+
+    // Capture stderr from the child process and send it to the client
+    createProcess1.stderr.on('data', (data) => {
+        console.error('stderr:', data);
+        res.write(data);
+    });
+
+    // Handle the child process exit event
+    createProcess1.on('exit', (code) => {
+        console.log(`Location file "Trajectory.csv" created successfully.`);
+        res.end(); // End the response once the process exits
+    });
+
+    const command2 = `/home/${username}/Desktop/gps-sdr-sim-gui-master/./gps-sdr-sim -x "/home/${username}/Desktop/Nettside/Trajectory.csv" -b 8 -d 100 -e "/home/${username}/Desktop/gps-sdr-sim-master/brdc1070.24n" -o "/home/${username}/Desktop/gps-sdr-sim-master/${filename}.bin"`;
+    const createProcess2 = exec(command2);
+
+    // Capture stdout from the child process and send it to the client
+    createProcess2.stdout.on('data', (data) => {
+        console.log('stdout:', data);
+        res.write(data);
+    });
+
+    // Capture stderr from the child process and send it to the client
+    createProcess2.stderr.on('data', (data) => {
+        console.error('stderr:', data);
+        res.write(data);
+    });
+
+    // Handle the child process exit event
+    createProcess2.on('exit', (code) => {
         console.log(`Location file ${filename}.bin created successfully.`);
         res.end(); // End the response once the process exits
     });
