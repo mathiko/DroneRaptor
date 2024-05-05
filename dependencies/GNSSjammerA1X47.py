3788c38984773c91f4fa47d1df6cb2f35f23120d
Mathias <132059440+mathiko@users.noreply.github.com>
2024-05-05 23:53:16 +0200

    Add files via upload

---
+++ dependencies/GNSSjammerA1X47.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#
+# SPDX-License-Identifier: GPL-3.0
+#
+# GNU Radio Python Flow Graph
+# Title: Not titled yet
+# GNU Radio version: 3.10.1.1
+
+from PyQt5 import Qt
+from gnuradio import analog
+from gnuradio import gr
+from gnuradio.filter import firdes
+from gnuradio.fft import window
+import sys
+import signal
+from argparse import ArgumentParser
+from gnuradio.eng_arg import eng_float, intx
+from gnuradio import eng_notation
+import osmosdr
+import time
+
+
+
+
+class FrequencyHoppingJammer(gr.top_block):
+
+    def __init__(self):
+        gr.top_block.__init__(self, "Not titled yet", catch_exceptions=True)
+
+        ##################################################
+        # Parameters
+        ##################################################
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.samp_rate = samp_rate = 20e6
+        self.freq = freq = 1389.225e6
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self.osmosdr_sink_0 = osmosdr.sink(
+            args="numchan=" + str(1) + " " + "hackrf=0000000000000000223c69dc2e48a64f"
+        )
+        self.osmosdr_sink_0.set_time_unknown_pps(osmosdr.time_spec_t())
+        self.osmosdr_sink_0.set_sample_rate(samp_rate)
+        self.osmosdr_sink_0.set_center_freq(freq, 0)
+        self.osmosdr_sink_0.set_freq_corr(0, 0)
+        self.osmosdr_sink_0.set_gain(14, 0)
+        self.osmosdr_sink_0.set_if_gain(47, 0)
+        self.osmosdr_sink_0.set_bb_gain(0, 0)
+        self.osmosdr_sink_0.set_antenna('', 0)
+        self.osmosdr_sink_0.set_bandwidth(20e6, 0)
+        self.analog_fastnoise_source_x_0 = analog.fastnoise_source_c(analog.GR_GAUSSIAN, 1, 0, 8192)
+
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.analog_fastnoise_source_x_0, 0), (self.osmosdr_sink_0, 0))
+
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.osmosdr_sink_0.set_sample_rate(self.samp_rate)
+
+    def get_freq(self):
+        return self.freq
+
+    def set_freq(self, freq):
+        self.freq = freq
+        self.osmosdr_sink_0.set_center_freq(self.freq, 0)
+
+
+
+
+def main(top_block_cls=FrequencyHoppingJammer, options=None):
+    qapp = Qt.QApplication(sys.argv)
+    tb = top_block_cls()
+
+    step = 20e6
+
+    def update_freq():
+        #access step as nonlocal variable
+        nonlocal step
+        #Update step value based on current frequency
+        current_freq = tb.get_freq()
+        if (current_freq == 1389.225e6 + 11*step):
+            step = -step
+
+        #Update frequency value
+        new_freq = current_freq + step
+        
+        # Set the new freq value
+        tb.set_freq(new_freq)
+        print(new_freq)
+        
+        
+    # Set up a QTimer to call update_freq() every 10 ms
+    timer = Qt.QTimer()
+    timer.timeout.connect(update_freq)
+    timer.start(1)
+
+
+    def sig_handler(sig=None, frame=None):
+        tb.stop()
+        tb.wait()
+
+        sys.exit(0)
+
+    signal.signal(signal.SIGINT, sig_handler)
+    signal.signal(signal.SIGTERM, sig_handler)
+
+    tb.start()
+
+    try:
+        input('Press Enter to quit: ')
+    except EOFError:
+        pass
+    tb.stop()
+    tb.wait()
+
+
+if __name__ == '__main__':
+    main()
